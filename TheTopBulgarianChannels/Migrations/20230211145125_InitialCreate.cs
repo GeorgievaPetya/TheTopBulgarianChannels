bbc67e2ccdbe068e3a1479f1cb76fdf29ab70a5b
GeorgievaP <you@example.com>
2023-02-11 16:58:26 +0200

    Change in the name of the server

---
+++ TheTopBulgarianChannels/Migrations/20230211145125_InitialCreate.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TheTopBulgarianChannels.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Countries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CountryName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Countries", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "YouTubeCnannels",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ChannelName = table.Column<string>(nullable: true),
+                    Subscribers = table.Column<int>(nullable: false),
+                    Views = table.Column<long>(nullable: false),
+                    ChannelUrl = table.Column<string>(nullable: true),
+                    CategoryId = table.Column<int>(nullable: true),
+                    CountryId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_YouTubeCnannels", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_YouTubeCnannels_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_YouTubeCnannels_Countries_CountryId",
+                        column: x => x.CountryId,
+                        principalTable: "Countries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_YouTubeCnannels_CategoryId",
+                table: "YouTubeCnannels",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_YouTubeCnannels_CountryId",
+                table: "YouTubeCnannels",
+                column: "CountryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "YouTubeCnannels");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Countries");
+        }
+    }
+}
