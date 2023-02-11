fd5e5fd47779aef82f6f00f4e4aebcb30beeeb8a
GeorgievaP <you@example.com>
2023-02-11 16:49:33 +0200

    Add services.AddDbContext

--- TheTopBulgarianChannels/Startup.cs
+++ TheTopBulgarianChannels/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TheTopBulgarianChannels.DataAccess;
 
 namespace TheTopBulgarianChannels
 {
@@ -24,6 +26,8 @@ namespace TheTopBulgarianChannels
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
