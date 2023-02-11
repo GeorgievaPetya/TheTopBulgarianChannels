5e6ab0ea820b5da4253eb604f3815109526f249c
GeorgievaP <you@example.com>
2023-02-11 17:22:32 +0200

    UpdateDataBase correction table name

---
+++ TheTopBulgarianChannels/Migrations/20230211152039_CorrectionToYouTubeChannelName2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TheTopBulgarianChannels.Migrations
+{
+    public partial class CorrectionToYouTubeChannelName2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
