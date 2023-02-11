003a37f3bbb527d018a0c613eecf830c4a0f88b8
GeorgievaP <you@example.com>
2023-02-11 15:26:24 +0200

    Add project files.

---
+++ TheTopBulgarianChannels/DataModels/YouTubeCnannel.cs
@@ -0,0 +1,16 @@
+﻿namespace TheTopBulgarianChannels.DataModels
+{
+    public class YouTubeCnannel
+    {
+        public int Id { get; set; }
+
+        public string ChannelName { get; set; }
+
+        public int Subscribers { get; set; }
+
+        public long Views { get; set; }  
+
+        public string ChannelUrl { get; set; } 
+
+    }
+}
