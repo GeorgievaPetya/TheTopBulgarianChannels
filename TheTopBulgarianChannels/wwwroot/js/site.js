27e02a0133df16fc16d37d6158d1ac731906e2d1
GeorgievaP <you@example.com>
2023-03-14 23:40:41 +0200

    Add Searching, Sorting, Paging in Facebook, Tiktok,Twitter

--- TheTopBulgarianChannels/wwwroot/js/site.js
+++ TheTopBulgarianChannels/wwwroot/js/site.js
@@ -1,6 +1,6 @@
 ﻿$(document).ready(function () {
-    $('#myTable').DataTable({
-        "scrollY": "450px",
+    var table = $('#myTableYouTube').DataTable({
+        "scrollY": "340px",
         "scrollCollapse": true,
         "paging": true,
         columnDefs: [
@@ -8,4 +8,68 @@
         ],
         order: [[1, 'asc']]
     });
+});
+$('#myTableYouTube tbody').on('click', 'tr', function () {
+    var data = table.row(this).data();
+    alert('You clicked on ' + data[0] + "'s row");
+});
+$(document).ready(function () {
+    var table = $('#myTableInstagram').DataTable({
+        "scrollY": "340px",
+        "scrollCollapse": true,
+        "paging": true,
+        columnDefs: [
+            { orderable: false, targets: 3 }
+        ],
+        order: [[1, 'asc']]
+    });
+});
+$('#myTableInstagram tbody').on('click', 'tr', function () {
+    var data = table.row(this).data();
+    alert('You clicked on ' + data[0] + "'s row");
+});
+$(document).ready(function () {
+    var table = $('#myTableTwitter').DataTable({
+        "scrollY": "340px",
+        "scrollCollapse": true,
+        "paging": true,
+        columnDefs: [
+            { orderable: false, targets: 3 }
+        ],
+        order: [[1, 'asc']]
+    });
+});
+$('#myTableTwitter tbody').on('click', 'tr', function () {
+    var data = table.row(this).data();
+    alert('You clicked on ' + data[0] + "'s row");
+});
+$(document).ready(function () {
+    var table = $('#myTableFacebook').DataTable({
+        "scrollY": "340px",
+        "scrollCollapse": true,
+        "paging": true,
+        columnDefs: [
+            { orderable: false, targets: 2 }
+        ],
+        order: [[1, 'asc']]
+    });
+});
+$('#myTableFacebook tbody').on('click', 'tr', function () {
+    var data = table.row(this).data();
+    alert('You clicked on ' + data[0] + "'s row");
+});
+$(document).ready(function () {
+    var table = $('#myTableTikTok').DataTable({
+        "scrollY": "340px",
+        "scrollCollapse": true,
+        "paging": true,
+        columnDefs: [
+            { orderable: false, targets: 3 }
+        ],
+        order: [[1, 'asc']]
+    });
+});
+$('#myTableTikTok tbody').on('click', 'tr', function () {
+    var data = table.row(this).data();
+    alert('You clicked on ' + data[0] + "'s row");
 });
